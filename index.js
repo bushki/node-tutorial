61ef9e6a2b7aa0c057fe997a40005795832a6584
Luis <luis.p.melgar@disney.com>
2019-02-18 19:31:02 -0800

    import class

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const person = require('./person');
-
+const Person = require('./person-class');
 console.log(person);
+
+//person from class
+const person1 = new Person('John', 45);
+person1.greeting();
