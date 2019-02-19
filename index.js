4498351a7e684d84885157a09f8554e6bda05bc2
Luis <luis.p.melgar@disney.com>
2019-02-18 21:13:02 -0800

    Events example using logger
    Added uuid package to generate GUID

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const person = require('./person');
-const Person = require('./person-class');
-console.log(person);
+// const person = require('./person');
+// const Person = require('./person-class');
+// console.log(person);
 
-//person from class
-const person1 = new Person('John', 45);
-person1.greeting();
+// //person from class
+// const person1 = new Person('John', 45);
+// person1.greeting();
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+logger.on('message', (data) => 
+console.log('Called Listener:',data)
+);
+
+logger.log('Hello logger!');
+logger.log('Hi');
+logger.log('Bye');
